714c1f0ba7fa822834c4839b770c2e13232bf18f
Hai Duong <149478749+HaixD@users.noreply.github.com>
2024-08-13 06:17:09 -0700

    add actions for verifying login and adding account

--- src/app/actions.ts
+++ src/app/actions.ts
@@ -1,6 +1,6 @@
 "use server"
 
-import { verifyLogin } from "@/app/api/database"
+import { verifyLogin, addAccount } from "@/app/api/database"
 
 export async function login(previousState: string, formData: FormData) {
     const username = formData.get("username")
@@ -9,9 +9,19 @@ export async function login(previousState: string, formData: FormData) {
     if (!username) return "No username was given"
     if (!password) return "No password was given"
 
-    console.log(verifyLogin(username.toString(), password.toString()))
+    if (!verifyLogin(username.toString(), password.toString())) return "The username and password do not match."
+    
+    return "success"
+}
 
-    if (username === "debug" || password === "debug") return "The username and password do not match."
+export async function createAccount(previousState: string, formData: FormData) {
+    const username = formData.get("username")
+    const password = formData.get("password")
+
+    if (!username) return "No username was given"
+    if (!password) return "No password was given"
+
+    if (!addAccount(username.toString(), password.toString())) return "Email already exists."
     
     return "success"
 }
--- src/app/api/database.ts
+++ src/app/api/database.ts
@@ -19,11 +19,29 @@ db.close()
 export function verifyLogin(email: string, password: string) {
     const db = new sqlite3.Database("plus.db")
 
-    const result = db.get(`
+    let found = false
+    db.get(`
         SELECT * FROM "accounts" WHERE ("email" = ?) AND ("password" = ?)
-    `, [email, password])
+    `, [email, password], (error, row: any) => {
+        if (!error && row.email && row.password) found = true
+    })
     
     db.close()
+    return found
+}
 
-    return result
+export function addAccount(email: string, password: string) {
+    const db = new sqlite3.Database("plus.db")
+
+    let success = false
+    db.get(`
+        INSERT INTO "accounts"
+        VALUES
+            (?, ?)
+    `, [email, password], (error, _) => {
+        if (!error) success = true
+    })
+    
+    db.close()
+    return success
 }
