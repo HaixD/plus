0fec004dbdadd357c032d8474f27d94af8c4d1ee
Hai Duong <149478749+HaixD@users.noreply.github.com>
2024-08-16 02:01:05 -0700

    home page now links to login

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,13 @@
+"use client"
+
+import { useEffect } from "react";
+
 export default function Home() {
+    
+    useEffect(() => {
+        window.location.replace("/login")
+    }, [])
+
   return (
     <div>PLACEHOLDER</div>
   );
