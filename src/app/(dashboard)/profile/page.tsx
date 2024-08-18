692162038d1a6745d2eb831e46dade8ced22b554
LUCAS JEONG <lucasjeong072@gmail.com>
2024-08-18 13:48:51 +0900

    this is the profile page in the home page

--- src/app/(dashboard)/profile/page.tsx
+++ src/app/(dashboard)/profile/page.tsx
@@ -1,5 +1,32 @@
+import profile from "./profile.module.css"
+import Image from "next/image"
+
 export default function Page() {
     return (
-        <div>u r trying to increase profile rizz</div>
+        <> <div>
+
+        <div className={profile.maincontainer}> 
+            <div className={profile.imagecontainer}> </div>
+                <Image className={profile.iconprofile} style= {{gridArea: "profpic"}}
+                src="/logo1.png" alt="logo" width={120} height={120} />
+
+                <Image className={profile.iconprofile} style= {{justifySelf: "flex-end", gridArea: "edit"}}
+                src="/edit.svg" alt="edit" width={30} height={30} />
+            
+                <p style={{gridArea: "username"}}>@Username</p> 
+
+                <p style={{gridArea: "bio"}}>Lorem ipsum dolor sit amet consectetur adipisciping elit. Magnam quis delectus volupatum.</p>
+
+                <p style={{gridArea: "date"}}>Joined 2024, August 16</p>
+        </div>
+
+        <div className={profile.buttoncontainer}>
+            <a href="placeholder1">Posts</a>
+            <a href="placeholder2">Likes</a>
+            <a href="placeholder3">Bookmarks</a>
+        </div>
+
+        </div>
+        </>
     )
 }
