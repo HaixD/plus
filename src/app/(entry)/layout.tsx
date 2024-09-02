38f18e5211d301c3ca22b685e5b91f377f432a91
Hai Duong <149478749+HaixD@users.noreply.github.com>
2024-09-01 17:55:22 -0700

    convert inline css to css for entry layout

--- src/app/(entry)/layout.tsx
+++ src/app/(entry)/layout.tsx
@@ -9,14 +9,7 @@ export default function Layout({
     children: ReactNode
 }>) {
     return (
-        <main className={styles.main}
-        //External CSS not applied correctly
-        style={{
-            minHeight: "100vh",
-            display: "grid",
-            gridTemplateColumns: "70vw auto"
-        }}
-        >
+        <main className={styles.main}>
             <Image 
                 src="/logo1.png" 
                 alt="logo" 
