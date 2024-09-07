a495d8e1856f520398fcc07747cd4abdd464867e
Hai Duong <149478749+HaixD@users.noreply.github.com>
2024-09-07 07:37:17 -0700

    fix lucas' db

--- src/app/api/database.ts
+++ src/app/api/database.ts
@@ -3,35 +3,43 @@ import sqlite3 from "sqlite3"
 export async function addToken(userID: number, token: string) {
     const db = new sqlite3.Database("plus.db")
     await new Promise<void>((resolve, reject) => {
-        db.get(`
+        db.get(
+            `
             INSERT INTO "tokens" ("token", "id")
             VALUES (?, ?)
             ON CONFLICT ("token") DO NOTHING
             RETURNING *
-        `, [token, userID], (error, row) => {
-            if (error || row === undefined) {
-                reject("Error occured when generating credentials")
-            } else {
-                resolve()
+        `,
+            [token, userID],
+            (error, row) => {
+                if (error || row === undefined) {
+                    reject("Error occured when generating credentials")
+                } else {
+                    resolve()
+                }
             }
-        })
+        )
     })
 }
 
 export async function updateToken(userID: number, token: string) {
     const db = new sqlite3.Database("plus.db")
     await new Promise<void>((resolve, reject) => {
-        db.run(`
+        db.run(
+            `
             DELETE FROM "tokens"
             WHERE "id" = ?
             RETURNING *
-        `, [userID], (error) => {
-            if (error) {
-                reject("Error occured when generating credentials")
-            } else {
-                resolve()
+        `,
+            [userID],
+            error => {
+                if (error) {
+                    reject("Error occured when generating credentials")
+                } else {
+                    resolve()
+                }
             }
-        })
+        )
     })
     await addToken(userID, token)
 }
@@ -39,34 +47,42 @@ export async function updateToken(userID: number, token: string) {
 export async function getUserID(token: string) {
     const db = new sqlite3.Database("plus.db")
     return await new Promise<number>((resolve, reject) => {
-        db.get(`
+        db.get(
+            `
             SELECT "id" from "tokens" WHERE "token" = ?
-        `, [token], (error, row: any) => {
-            if (error) {
-                reject("Error occured while accessing user information")
-            } else if (row === undefined) {
-                reject("Credentials have expired")
-            } else {
-                resolve(row.id)
+        `,
+            [token],
+            (error, row: any) => {
+                if (error) {
+                    reject("Error occured while accessing user information")
+                } else if (row === undefined) {
+                    reject("Credentials have expired")
+                } else {
+                    resolve(row.id)
+                }
             }
-        })
+        )
     })
 }
 
 export async function verifyLogin(username: string, password: string) {
     const db = new sqlite3.Database("plus.db")
     return await new Promise<number>((resolve, reject) => {
-        db.get(`
+        db.get(
+            `
             SELECT "id" FROM "accounts" WHERE ("username" = ?) AND ("password" = ?)
-        `, [username, password], (error, row: any) => {
-            if (error) {
-                reject("Error occured when searching for account")
-            } else if (row !== undefined) {
-                resolve(row.id)
-            } else {
-                reject("Invalid password for the given username")
+        `,
+            [username, password],
+            (error, row: any) => {
+                if (error) {
+                    reject("Error occured when searching for account")
+                } else if (row !== undefined) {
+                    resolve(row.id)
+                } else {
+                    reject("Invalid password for the given username")
+                }
             }
-        })
+        )
         db.close()
     })
 }
@@ -74,38 +90,48 @@ export async function verifyLogin(username: string, password: string) {
 export async function addAccount(username: string, password: string) {
     const db = new sqlite3.Database("plus.db")
     return await new Promise<number>((resolve, reject) => {
-    
         db.serialize(() => {
-            db.get(`
+            let userID: number = -1
+            db.get(
+                `
                 INSERT INTO "accounts" ("username", "password")
                 VALUES
                     (?, ?)
                 ON CONFLICT DO NOTHING
                 RETURNING "id"
-            `, [username, password], (error, row: any) => {
-                if (error) {
-                    reject("Error occured when adding account")
-                } else if (row !== undefined) {
-                    resolve(row.id)
-                } else {
-                    reject("Username already exists")
+            `,
+                [username, password],
+                (error, row: any) => {
+                    if (error) {
+                        reject("Error occured when adding account")
+                    } else if (row !== undefined) {
+                        userID = row.id
+                    } else {
+                        reject("Username already exists")
+                    }
                 }
-            })
-            const userId = username
-            db.run(`
+            )
+
+            if (userID === -1) return
+
+            db.run(
+                `
                 INSERT INTO "profile" ("id", "bio", "picture")
                 VALUES (?, '', '/default.png')
                 ON CONFLICT ("id") DO NOTHING
                 RETURNING *
-        `   , [userId], (error: any, row: any) => {
-            if (error) {
-                reject("Error occured when creating Biography")
-            } else if (row === undefined) {
-                reject("User already has existing biography")
-            } else {
-                resolve()
-            }
-        })
+        `,
+                [userID],
+                (error: any, row: any) => {
+                    if (error) {
+                        reject("Error occured when creating Biography")
+                    } else if (row === undefined) {
+                        reject("User already has existing biography")
+                    } else {
+                        resolve(userID)
+                    }
+                }
+            )
         })
         db.close()
     })
@@ -113,106 +139,135 @@ export async function addAccount(username: string, password: string) {
 
 export async function changeUsername(token: string, newUsername: string) {
     const posterID = await getUserID(token)
-    
+
     const db = new sqlite3.Database("plus.db")
     await new Promise<void>((resolve, reject) => {
-        db.get(`
+        db.get(
+            `
             UPDATE "accounts"
             SET
                 "username" = ?
             WHERE "id" = ?
             RETURNING *
-        `, [newUsername, posterID], (error, row: any) => {
-            if (error) {
-                if ("errno" in error && error.errno === 19) {
-                    reject("Username is already taken")
-                } else {
+        `,
+            [newUsername, posterID],
+            (error, row: any) => {
+                if (error) {
+                    if ("errno" in error && error.errno === 19) {
+                        reject("Username is already taken")
+                    } else {
+                        reject("Error occured when changing account username")
+                    }
+                } else if (row === undefined) {
                     reject("Error occured when changing account username")
+                } else {
+                    resolve()
                 }
-            } else if (row === undefined) {
-                reject("Error occured when changing account username")
-            } else {
-                resolve()
             }
-        })
+        )
         db.close()
     })
 }
 
 export async function changePassword(token: string, newPassword: string) {
     const posterID = await getUserID(token)
-    
+
     const db = new sqlite3.Database("plus.db")
     await new Promise<void>((resolve, reject) => {
-        db.get(`
+        db.get(
+            `
             UPDATE "accounts"
             SET
                 "password" = ?
             WHERE "id" = ?
             RETURNING *
-        `, [newPassword, posterID], (error, row: any) => {
-            if (error || row === undefined) {
-                reject("Error occured when changing account username")
-            } else {
-                resolve()
+        `,
+            [newPassword, posterID],
+            (error, row: any) => {
+                if (error || row === undefined) {
+                    reject("Error occured when changing account username")
+                } else {
+                    resolve()
+                }
             }
-        })
+        )
         db.close()
     })
 }
 
-export async function addPost(token: string, content: string | null, imagePath: string | null) {
+export async function addPost(
+    token: string,
+    content: string | null,
+    imagePath: string | null
+) {
     const posterID = await getUserID(token)
 
     const db = new sqlite3.Database("plus.db")
     await new Promise<void>((resolve, reject) => {
-        db.run(`
+        db.run(
+            `
             INSERT INTO "posts" ("poster", "content", "image_path")
             VALUES (?, ?, ?)
-        `, [posterID, content, imagePath], error => {
-            if (error) {
-                reject("Error occured when adding post")
-            } else {
-                resolve()
+        `,
+            [posterID, content, imagePath],
+            error => {
+                if (error) {
+                    reject("Error occured when adding post")
+                } else {
+                    resolve()
+                }
             }
-        })
+        )
         db.close()
     })
 }
 
-//this also is not finished, make it so there is an option to save the profile picture as well. 
-export async function addBioModal(token: string, bio: string, imagePath: string | null) {
+//this also is not finished, make it so there is an option to save the profile picture as well.
+export async function addBioModal(
+    token: string,
+    bio: string,
+    imagePath: string | null
+) {
     const userID = await getUserID(token)
-    
+
     const db = new sqlite3.Database("plus.db")
     await new Promise<void>((resolve, reject) => {
         //not sure if this has to be in order for the columns
-        db.run(`
+        db.run(
+            `
             INSERT INTO "profile" ("id", "bio", "picture")
             VALUES (?, ?, ?)
             ON CONFLICT ("id") DO NOTHING
             RETURNING *
-        `, [bio, imagePath], (error: any, row: any) => {
-            if (error) {
-                reject("Error occured when creating Biography")
-            } else if (row === undefined) {
-                reject("User already has existing biography")
-            } else {
-                resolve()
+        `,
+            [bio, imagePath],
+            (error: any, row: any) => {
+                if (error) {
+                    reject("Error occured when creating Biography")
+                } else if (row === undefined) {
+                    reject("User already has existing biography")
+                } else {
+                    resolve()
+                }
             }
-        })
+        )
         db.close()
     })
 }
 
 // this isn't finished,  and I'm frankly confused
-export async function changeBioModal(token: string, bio: string | null, imagePath: string | null) {
+export async function changeBioModal(
+    token: string,
+    bio: string | null,
+    imagePath: string | null
+) {
     //not sure what to do with this as ID has to be matched to the profile
     const userID = await getUserID(token)
-    
+
     const db = new sqlite3.Database("plus.db")
     await new Promise<void>((resolve, reject) => {
-        db.get(`
+        db.get(
+            `
             INSERT INTO 
                 profiles ("bio", "picture", "id")
                 VALUES (?, ?, ?)
@@ -220,16 +275,18 @@ export async function changeBioModal(token: string, bio: string | null, imagePat
                    bio = EXCLUDED.bio,
                    picture = EXCLUDED.picture
                 RETURNING *
-        `, [bio, imagePath, userID], (error, row: any) => {
+        `,
+            [bio, imagePath, userID],
+            (error, row: any) => {
+                console.log(error, row)
 
-            console.log(error, row)
-
-            if (row === undefined) {
-                reject("Error. changes were not saved.")
-            } else {
-                resolve()
+                if (row === undefined) {
+                    reject("Error. changes were not saved.")
+                } else {
+                    resolve()
+                }
             }
-        })
+        )
         db.close()
     })
 }
