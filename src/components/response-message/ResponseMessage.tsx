743ae07506ad3ecc3e63de2a6f0229e0888563fd
Hai Duong <149478749+HaixD@users.noreply.github.com>
2024-09-15 01:24:21 -0700

    fix removal of error response in ResponseMessage

--- src/components/response-message/ResponseMessage.tsx
+++ src/components/response-message/ResponseMessage.tsx
@@ -1,8 +1,7 @@
-import { ErrorResponse } from "@/app/actions"
 import styles from "./styles.module.css"
 import { HTMLAttributes } from "react"
 
-type Response<T> = T | ErrorResponse
+type Response<T> = T | { error: string }
 
 type ResponseMessageProps<T extends object> = {
     responseState?: Response<T>
@@ -11,16 +10,20 @@ type ResponseMessageProps<T extends object> = {
 
 export function ResponseMessage<T extends object>({
     responseState,
-    successMessage="Success",
-    id="",
+    successMessage = "Success",
+    id = "",
     ...props
 }: Readonly<ResponseMessageProps<T>>) {
     if (!responseState) return <p className={styles["response-message"]}></p>
-    
+
     const isError = "error" in responseState
-    
+
     return (
-        <p id={isError ? styles["error-message"] : styles["success-message"]} className={styles["response-message"]} {...props}>
+        <p
+            id={isError ? styles["error-message"] : styles["success-message"]}
+            className={styles["response-message"]}
+            {...props}
+        >
             {isError ? responseState.error : successMessage}
         </p>
     )
