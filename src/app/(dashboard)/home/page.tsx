9893e0b142205c56d1cd1153437d2945ac19398e
Hai Duong <149478749+HaixD@users.noreply.github.com>
2024-10-03 13:21:45 -0700

    add react-icons library and partially implement post component

--- src/app/(dashboard)/home/page.tsx
+++ src/app/(dashboard)/home/page.tsx
@@ -1,13 +1,18 @@
 import Image from "next/image"
 import styles from "./styles.module.css"
+import { Post } from "@/components/post/Post"
+import { getPost } from "@/actions/posts"
 
-export default function Page() {
+export default async function Page() {
+    const content = await getPost()
+
     return (
         <div>
             <div id={styles.logo}>
                 <Image src="/logo1.png" alt="logo" width={60} height={60} />
                 <p>For You</p>
             </div>
+            <Post {...content[0]} />
         </div>
     )
 }
--- src/components/post/Post.tsx
+++ src/components/post/Post.tsx
@@ -1,17 +1,78 @@
-export type PostProps = {
-    profilePicture: string
-    username: string
-    content: string
-    likes: number
-    image: string
-}
+"use client"
+import * as FaIcons from "react-icons/fa"
+import Image from "next/image"
+import styles from "./styles.module.css"
+import { getPost } from "@/actions/posts"
+import { DOMAttributes, useEffect, useState } from "react"
+
+export type PostProps = Awaited<ReturnType<typeof getPost>>[number]
 
 export function Post({
-    profilePicture,
     username,
+    pfp,
+    img: image,
     content,
-    likes,
-    image,
 }: Readonly<PostProps>) {
-    return <div></div>
+    const expandPost = () => {}
+    const likePost = () => {}
+    const bookmarkPost = () => {}
+
+    return (
+        <div className={styles.post}>
+            {pfp ? (
+                <Image src={pfp} alt="" id={styles.pfp} />
+            ) : (
+                <div id={styles.pfp} />
+            )}
+            <p id={styles.username}>{username}</p>
+            <p id={styles.content}>{content}</p>
+            {image ? <Image src={image} alt="" id={styles.content} /> : null}
+            <div id={styles.interaction}>
+                <Interaction untoggledIcon="FaRegComment" />
+                <Interaction untoggledIcon="FaRegHeart" toggledIcon="FaHeart" />
+                <Interaction
+                    untoggledIcon="FaRegBookmark"
+                    toggledIcon="FaBookmark"
+                />
+            </div>
+        </div>
+    )
+}
+
+function Interaction({
+    untoggledIcon,
+    toggledIcon,
+    onClick,
+    initialState = false,
+}: Readonly<{
+    onClick?: DOMAttributes<HTMLButtonElement>["onClick"]
+    untoggledIcon: keyof typeof FaIcons
+    toggledIcon?: keyof typeof FaIcons
+    initialState?: boolean
+}>) {
+    const [toggled, setToggled] = useState(false)
+
+    useEffect(() => {
+        setToggled(initialState)
+    }, [initialState])
+
+    const UntoggledElement = FaIcons[untoggledIcon]
+    const ToggledElement = toggledIcon ? FaIcons[toggledIcon] : null
+
+    return (
+        <button
+            onClick={evt => {
+                setToggled(!toggled)
+
+                if (onClick) onClick(evt)
+            }}
+            className={styles.icon}
+        >
+            {toggled && ToggledElement ? (
+                <ToggledElement />
+            ) : (
+                <UntoggledElement />
+            )}
+        </button>
+    )
 }
